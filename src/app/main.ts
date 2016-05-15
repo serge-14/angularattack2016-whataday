ce6a5d6ffbda31392ec7ae8707ea3a661234d980
Renat Valeev <renat.valeev@gmail.com>
2016-05-15 23:42:49 +0300

    enable routing

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,9 +5,10 @@ import {AppComponent} from './app.component';
 import {Production} from './enviroment';
 import {enableProdMode} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
+import {ROUTER_PROVIDERS} from "@angular/router";
 
 if (Production) {
     enableProdMode();
 }
 
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
